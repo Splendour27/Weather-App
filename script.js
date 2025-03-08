909be352a9c201dc3b4da962125928b1a2414faa
Fedora Chiboka <doraline62@gmail.com>
2025-03-08 10:09:04 +0100

    html,css,js

---
+++ script.js
@@ -0,0 +1,34 @@
+let cityInput =
+document.querySelector("#cityInput");
+let getWeatherBtn =
+document.querySelector("getWeatherBtn");
+
+const apiKey= "2f8ee0ef02efee4839075a7d7189e73"
+
+async function fetchWeather(city) {
+    try {
+        let response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+        );
+        let data = await response.json();
+
+        if (response.ok) {
+            displayWeather(data);
+        }
+        
+        else {
+            weatherInfo.innerHTML = `<p style="color:red;">City not found. Try again!</p>`;
+        }
+    } 
+    catch  (error) {
+        conhsole.log("Error fetching weather data:", error);
+        weatherInfo.innerHTML = `<p style="color:red;">Something went wrong. Try again later.</p>`;
+    }}
+
+    function displayWeather(data) {
+        weatherInfo.innerHTML = `
+            <h2>Weather in ${data.name}</h2>
+            <p> Temperature: ${data.main.temp}°C</p>
+            <p> Humidity: ${data.main.humidity}%</p>
+            <p> Condition: ${data.weather[0].description}</p>
+        `;}
